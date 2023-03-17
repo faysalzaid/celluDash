341020b0fa40b750651edad5ac181aa157dc0c65
Jabir Omar <jabiromar400@gmail.com>
2023-03-17 07:16:38 +0300

    updated

--- src/components/navbar/Theme.js
+++ src/components/navbar/Theme.js
@@ -12,7 +12,7 @@ function Theme() {
     }
 
   return (
-    <div id='theme-container' className=' flex items-center justify-between pl-[5px] pr-[5px] mr-3 bg-white h-[30px] w-[65px] relative rounded-lg shadow-shadow1 cursor-pointer' onClick={handleTheme}>
+    <div id='theme-container' className=' flex items-center justify-between pl-[5px] pr-[5px] sm:mr-0 md:mr-3 bg-white h-[30px] w-[65px] relative rounded-lg shadow-shadow1 cursor-pointer' onClick={handleTheme}>
         <BsSunFill className=' text-lg text-black' />
         <i id='indicator' className=' w-[30px] h-[30px] bg-orange-500 absolute rounded-lg scale-[0.84] left-0 duration-100'></i>
         <BsMoon />
