f230847ee5c1aa0cbce5c902ee5e8548b64a87d6
kaleb <kalebmelaku97@gmail.com>
2023-03-27 23:09:21 +0300

    chat section

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -40,7 +40,7 @@ export default function Chat() {
                 <div className="flex flex-col flex-grow w-full max-w-xl bg-gray-100 dark:bg-zinc-800 shadow-xl rounded-lg overflow-hidden">
                     <div id='chatContainer' className="flex flex-col flex-grow h-0 p-4 overflow-auto">
                         <div className="flex w-full mt-2 space-x-3 max-w-xs">
-                            {/* <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div> */}
+                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
                             <div>
                                 <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
                                     <p className="text-sm">Hello Sir!</p>
